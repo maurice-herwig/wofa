e16140a68c647a030b318327e203bf33846ddcc8
maurice-herwig <uk061641@student.uni-kassel.de>
2022-02-16 08:51:41 +0100

    add variants to examples

--- Example.py
+++ Example.py
@@ -30,13 +30,18 @@ automaton6 = FiniteAutomata({1}, [(1, 'a', 2), (1, 'b', 1), (2, 'a', 2), (2, 'b'
                                   (4, 'a', 5), (4, 'b', 4), (5, 'a', 5), (5, 'b', 5)], {5})
 
 
-def example_sym_diff():
+def example_sym_diff(variant):
     """ This is an example of how weighting regular languages can be used in teaching. This example should clarify that
     with the weighting of the symmetrical difference between the submission and a sample solution, the quality of the
     submission can be determined. It should be noted that the lower the weight, the closer the language of the
     submission is to the language of the solution.
+
+    Args:
+        variant (string):   Determines the variant of how the words in the constant part are redistributed.
+                            'words' := All words in the constant part have the same weight.
+                            'wordLengths' := All word lengths in the constant part have the same weight.
     """
-    print("Example weight of the symmetrical difference:")
+    print(f'Example weight of the symmetrical difference. Variant = {variant}:')
 
     # Determine the parameters. x is the proportion of the weight to be allocated to the constant part.
     x = 0.5
@@ -44,21 +49,26 @@ def example_sym_diff():
     lam = (1 - x) ** (1 / eta)
 
     # Determine the weight of the symmetrical difference and then print the result.
-    print(f'Weight diff. Automata 1 to Solution = {weight_diff(sol, automaton1, eta, lam)[2]}')
-    print(f'Weight diff. Automata 2 to Solution = {weight_diff(sol, automaton2, eta, lam)[2]}')
-    print(f'Weight diff. Automata 3 to Solution = {weight_diff(sol, automaton3, eta, lam)[2]}')
-    print(f'Weight diff. Automata 4 to Solution = {weight_diff(sol, automaton4, eta, lam)[2]}')
-    print(f'Weight diff. Automata 5 to Solution = {weight_diff(sol, automaton5, eta, lam)[2]}')
-    print(f'Weight diff. Automata 6 to Solution = {weight_diff(sol, automaton6, eta, lam)[2]}')
+    print(f'Weight diff. Automata 1 to Solution = {weight_diff(sol, automaton1, eta, lam, variant)[2]}')
+    print(f'Weight diff. Automata 2 to Solution = {weight_diff(sol, automaton2, eta, lam, variant)[2]}')
+    print(f'Weight diff. Automata 3 to Solution = {weight_diff(sol, automaton3, eta, lam, variant)[2]}')
+    print(f'Weight diff. Automata 4 to Solution = {weight_diff(sol, automaton4, eta, lam, variant)[2]}')
+    print(f'Weight diff. Automata 5 to Solution = {weight_diff(sol, automaton5, eta, lam, variant)[2]}')
+    print(f'Weight diff. Automata 6 to Solution = {weight_diff(sol, automaton6, eta, lam, variant)[2]}')
     print()
 
 
-def example_weight_lang():
+def example_weight_lang(variant):
     """ This example shows how to calculate for different automata the weight of their language. 
         The same automata are used here as in the previous example. 
         In docs/ExampleAutomates.pdf are graphical representations of these finite automata
+
+    Args:
+        variant (string):   Determines the variant of how the words in the constant part are redistributed.
+                            'words' := All words in the constant part have the same weight.
+                            'wordLengths' := All word lengths in the constant part have the same weight.
     """
-    print("Example weight of a language:")
+    print(f'Example weight of a language. Variant = {variant}:')
 
     # Setting the alphabet.
     FiniteAutomata.set_alphabet({'a', 'b'})
@@ -76,16 +86,16 @@ def example_weight_lang():
 
         # Determination of weights and output of results.
         if i < 1:
-            print(f'Weight Solution = {weight(automatas[i].determine(), eta, lam)}')
+            print(f'Weight Solution = {weight(automatas[i].determine(), eta, lam, variant)}')
         else:
-            print(f'Weight Automata {i} = {weight(automatas[i].determine(), eta, lam)}')
+            print(f'Weight Automata {i} = {weight(automatas[i].determine(), eta, lam, variant)}')
     print()
 
 
 def example_same_weight():
     """ This example should make clear that the weight of two languages is not an indicator of how far apart they are,
-    but this can only be determined by weighting the symmetrical difference. Here you can see that the languages
-    a^* and b^* have the same weight, but the weight of the symmetric difference is not equal to 0.
+        but this can only be determined by weighting the symmetrical difference. Here you can see that the languages
+        a^* and b^* have the same weight, but the weight of the symmetric difference is not equal to 0.
     """
     print("Example same weight but weight Symmetric difference not 0.")
 
@@ -109,14 +119,19 @@ def example_same_weight():
     print()
 
 
-def example_eta():
+def example_eta(variant):
     """ This example shows the influence of the parameter eta on the resulting weight of a language. In general it can
     be said that it makes sense to set eta to the pumping constant of the language of the automaton. The language of
     this example automaton (the language which does not contain the subword "abba") is also an example why it can make
     sense in some cases to choose eta differently, because up to and including the word length 4 there is only one word
     above the alphabet which is not in the language.
+
+    Args:
+        variant (string):   Determines the variant of how the words in the constant part are redistributed.
+                            'words' := All words in the constant part have the same weight.
+                            'wordLengths' := All word lengths in the constant part have the same weight.
     """
-    print("Example of different values for eta.")
+    print(f'Example of different values for eta. Variant = {variant}:')
     # Setting the alphabet.
     FiniteAutomata.set_alphabet({'a', 'b'})
 
@@ -131,19 +146,24 @@ def example_eta():
     eta_values = [i for i in range(0, 11)] + [15, 20, 25, 30, 40, 50, 100, 200, 500]
     for eta in eta_values:
         s = f'Weight for eta={eta}'.ljust(20)
-        s += f'= {weight(automaton, eta, lam)}'
+        s += f'= {weight(automaton, eta, lam, variant)}'
         print(s)
     print()
 
 
-def example_lambda():
+def example_lambda(variant):
     """ This example shows how the parameter lambda influences the weight of a language. In general, a small value of
     lambda leads to a higher weighting of shorter word lengths while larger values lead to the fact that also the longer
     word lengths flow more into the weighting. It is interesting with the example automata (the language which does not
     contain the subword "abba") used here that for the language for longer word lengths only very few words contain,
     whereby by a higher value for Lambda the weight strongly decreases.
+
+    Args:
+        variant (string):   Determines the variant of how the words in the constant part are redistributed.
+                            'words' := All words in the constant part have the same weight.
+                            'wordLengths' := All word lengths in the constant part have the same weight.
     """
-    print("Example of different values for lambda.")
+    print(f'Example of different values for lambda. Variant = {variant}:')
     # Setting the alphabet.
     FiniteAutomata.set_alphabet({'a', 'b'})
 
@@ -158,36 +178,51 @@ def example_lambda():
     lam_values = [i / 20 for i in range(1, 20)] + [0.96, 0.97, 0.98, 0.99]
     for lam in lam_values:
         s = f'Weight for lambda={lam}'.ljust(23)
-        s += f'= {weight(automaton, eta, lam)}'
+        s += f'= {weight(automaton, eta, lam, variant)}'
         print(s)
     print()
 
 
-def example_visualisation():
+def example_visualisation(variant):
     """ This method shows with a visualization how the weight is changed by the parameters. For the automaton used as a
     solution in the previous examples, the weight is calculated for different combinations of eta and lambda and
     displayed graphically.
+
+    Args:
+        variant (string):   Determines the variant of how the words in the constant part are redistributed.
+                            'words' := All words in the constant part have the same weight.
+                            'wordLengths' := All word lengths in the constant part have the same weight.
     """
     print("Wait a moment please for creating the visualisation....")
 
     # Visualisation of the weight for difference eta and lambda
-    vis_weight(sol.determine(), np.arange(0, 11), 20, 'heatmap')
+    vis_weight(sol.determine(), np.arange(0, 11), 20, 'heatmap', variant)
 
 
-def example_visualisation_diff():
+def example_visualisation_diff(variant):
     """ This method shows with a visualization how the weight is changed by the parameters. For the automaton used as
     solution in the previous examples and the automaton named automaton 3, the weight of the symmetric difference for
     the different combination of eta and lambda is calculated and displayed graphically.
+
+    Args:
+        variant (string):   Determines the variant of how the words in the constant part are redistributed.
+                            'words' := All words in the constant part have the same weight.
+                            'wordLengths' := All word lengths in the constant part have the same weight.
     """
     print("Wait a moment please for creating the visualisation....")
 
     # Visualisation of the weight for difference eta and lambda
-    vis_diff(sol, automaton3, np.arange(0, 11), 20, 'surface')
+    vis_diff(sol, automaton3, np.arange(0, 11), 20, 'surface', variant)
 
 
-def example_find_best_parameters():
+def example_find_best_parameters(variant):
     """This example shows how the automates from example_sym_diff behave with different parameter values.
     This example can help to find the best parameters for the use case of evaluating student submissions.
+
+    Args:
+        variant (string):   Determines the variant of how the words in the constant part are redistributed.
+                            'words' := All words in the constant part have the same weight.
+                            'wordLengths' := All word lengths in the constant part have the same weight.
     """
     print("Wait a moment please for creating the visualisation....")
 
@@ -210,28 +245,33 @@ def example_find_best_parameters():
 
     # Create the surfaces for the weights of the symmetrical difference between the solution and the 6 different
     # automatas from students submissions.
-    ax.plot_surface(x, y, weight_values(sol.symmetric_difference(automaton1), etas, lams), cmap='Greys',
+    ax.plot_surface(x, y, weight_values(sol.symmetric_difference(automaton1), etas, lams, variant), cmap='Greys',
                     edgecolor='grey')
-    ax.plot_surface(x, y, weight_values(sol.symmetric_difference(automaton2), etas, lams), cmap='Purples',
+    ax.plot_surface(x, y, weight_values(sol.symmetric_difference(automaton2), etas, lams, variant), cmap='Purples',
                     edgecolor='purple')
-    ax.plot_surface(x, y, weight_values(sol.symmetric_difference(automaton3), etas, lams), cmap='Greens',
+    ax.plot_surface(x, y, weight_values(sol.symmetric_difference(automaton3), etas, lams, variant), cmap='Greens',
                     edgecolor='green')
-    ax.plot_surface(x, y, weight_values(sol.symmetric_difference(automaton4), etas, lams), cmap='Oranges',
+    ax.plot_surface(x, y, weight_values(sol.symmetric_difference(automaton4), etas, lams, variant), cmap='Oranges',
                     edgecolor='orange')
-    ax.plot_surface(x, y, weight_values(sol.symmetric_difference(automaton5), etas, lams), cmap='BuPu',
+    ax.plot_surface(x, y, weight_values(sol.symmetric_difference(automaton5), etas, lams, variant), cmap='BuPu',
                     edgecolor='blue')
-    ax.plot_surface(x, y, weight_values(sol.symmetric_difference(automaton6), etas, lams), cmap='BuGn',
+    ax.plot_surface(x, y, weight_values(sol.symmetric_difference(automaton6), etas, lams, variant), cmap='BuGn',
                     edgecolor='green')
 
     plt.show()
 
 
-def example_tikz():
+def example_tikz(variant):
     """This example shows how to create the graphical representation as surface from the previous examples
     as tikz picture.
+
+    Args:
+        variant (string):   Determines the variant of how the words in the constant part are redistributed.
+                            'words' := All words in the constant part have the same weight.
+                            'wordLengths' := All word lengths in the constant part have the same weight.
     """
     print("Please wait a moment the file will be created")
-    surface_to_tikz(sol.symmetric_difference(automaton4), np.arange(0, 15), 30, "assets/surface")
+    surface_to_tikz(sol.symmetric_difference(automaton4), np.arange(0, 15), 30, "assets/surface", variant)
     print("The file was created.")
 
 
@@ -241,14 +281,29 @@ if __name__ == "__main__":
 
     """Here you can comment out which examples should be executed when executing this file.
     """
-    """
-    example_sym_diff()
-    example_weight_lang()
+
+    example_sym_diff('words')
+    example_sym_diff('wordLengths')
+
+    example_weight_lang('words')
+    example_weight_lang('wordLengths')
+
     example_same_weight()
-    example_eta()
-    example_lambda()
-    example_visualisation()
-    example_visualisation_diff()
-    example_find_best_parameters()
-    """
-    example_tikz()
+
+    example_eta('words')
+    example_eta('wordLengths')
+
+    example_lambda('words')
+    example_lambda('wordLengths')
+
+    # example_visualisation('words')
+    example_visualisation('wordLengths')
+
+    example_visualisation_diff('words')
+    # example_visualisation_diff('wordLengths')
+
+    example_find_best_parameters('words')
+    # example_find_best_parameters('wordLengths')
+
+    # example_tikz('words')
+    # example_tikz('wordLengths')
