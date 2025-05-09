ff0fc70c716413738c4cc01382173d63ea8ab022
Maurice Herwig <maurice.herwig@uni-kassel.de>
2025-05-09 14:26:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -74,6 +74,7 @@ setup(
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
+    package_data={NAME: ['assets/*']},
     license='MIT',
     classifiers=[
         # Trove classifiers
