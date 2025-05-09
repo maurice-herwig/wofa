c29a6e3f383b8f5b7bb61a94481dd7bf51aeb3da
Maurice Herwig <maurice.herwig@uni-kassel.de>
2025-05-09 14:16:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(exclude=["assets", "docs", "tests", "*.tests", "*.tests.*", "tests.*"]),
+    packages=find_packages(exclude=["docs", "tests", "*.tests", "*.tests.*", "tests.*"]),
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['wofa'],
 
